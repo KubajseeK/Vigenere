93138f6ce38882ef55306d17b2043abe563dae12
Kubik <jacobkutka@gmail.com>
2020-02-06 13:58:43 +0100

    Classwork

---
+++ src/sample/MyFile.java
@@ -0,0 +1,40 @@
+package sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MyFile {
+        public String readFile(String sourceFile) {
+            String data = "";
+            try {
+                File myFile = new File(sourceFile);
+                Scanner myReader = new Scanner(myFile);
+                while (myReader.hasNextLine()) {
+                    data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        public void writeToFile(String sourceFile, String file) throws IOException {
+            try {
+                File newFile = new File(file);
+                FileWriter fw = new FileWriter(newFile);
+                fw.write(readFile(sourceFile));
+                fw.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+}
